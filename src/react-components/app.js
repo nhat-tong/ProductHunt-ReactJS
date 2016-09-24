b54890525dee451b1daf046e9b91be637773642e
Hoang Quoc Nhat TONG <htong@expertime.com>
2016-09-24 15:50:12 +0200

    Init ProductHunt-ReactJS project

---
+++ src/react-components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './HomePage';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <HomePage/>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
