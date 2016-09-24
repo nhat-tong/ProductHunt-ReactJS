c81cf0c08278497ae0c9b4db6b509c64915df794
Hoang Quoc Nhat TONG <htong@expertime.com>
2016-09-24 22:35:56 +0200

    Add

--- src/react-components/HomePage/index.js
+++ src/react-components/HomePage/index.js
@@ -3,6 +3,34 @@ import React from 'react';
 class HomePage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      ProductList: [
+        {
+          id: 1,
+          name: 'CodeCademy',
+          link: 'https://codecademy.com',
+          media: '/img/codecademy.jpeg',
+          upvote: 169,
+          description: 'Code for anyone',
+          marker: {
+            name: 'hieu',
+            avatar: '/img/hieu.jpeg'
+        }
+      },
+        {
+          id: 2,
+          name: 'Code4Startup',
+          link: 'https://code4startup.com',
+          media: '/img/code4startup.jpeg',
+          upvote: 169,
+          description: 'Code for startups',
+          marker: {
+            name: 'leo',
+            avatar: '/img/leo.jpeg'
+        }
+      }
+      ]
+    };
   }
 
   render() {
@@ -14,7 +42,58 @@ class HomePage extends React.Component {
 
         <section>
           <section className="container">
-            <h2>Product List</h2>
+
+            <ul className="product-list">
+              <li className="product-item">
+                <a className="upvote-button" href="#">
+                  <span>
+                    <i className="fa fa-sort-asc"></i>
+                  </span>
+                  {this.state.ProductList[0].upvote}
+                </a>
+                <img className="product-item-media" src={this.state.ProductList[0].media} />
+                <section className="product-item-info">
+                  <a href="#">
+                    <h2>{this.state.ProductList[0].name}</h2>
+                  </a>
+                  <p>{this.state.ProductList[0].description}</p>
+                    <a href="#">
+                      <img className="small-avatar" src={this.state.ProductList[0].marker.avatar} />
+                    </a>
+                </section>
+                <a className="product-item-link" href={this.state.ProductList[0].link}>
+                  <span>
+                    <i className="fa fa-external-link"></i>
+                  </span>
+                </a>
+              </li>
+
+              <li className="product-item">
+                <a className="upvote-button" href="#">
+                  <span>
+                    <i className="fa fa-sort-asc"></i>
+                  </span>
+                  {this.state.ProductList[1].upvote}
+                </a>
+                <img className="product-item-media" src={this.state.ProductList[1].media} />
+                <section className="product-item-info">
+                  <a href="#">
+                    <h2>{this.state.ProductList[1].name}</h2>
+                  </a>
+                  <p>{this.state.ProductList[1].description}</p>
+                    <a href="#">
+                      <img className="small-avatar" src={this.state.ProductList[1].marker.avatar} />
+                    </a>
+                </section>
+                <a className="product-item-link" href={this.state.ProductList[1].link}>
+                  <span>
+                    <i className="fa fa-external-link"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+
+
           </section>
         </section>
       </section>
